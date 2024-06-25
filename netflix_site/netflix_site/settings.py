93d3296b72428f468035955f3aa4a97a1a93c6e5
Knowledgex187 <bizcuitx167@hotmail.co.uk>
2024-06-25 09:29:13 +0100

    Created requirements.txt via pip freeze > requirements.txt. Added settings for cookies and conneciton to be over HTTPS only. Created a Procfile.

--- netflix_site/netflix_site/settings.py
+++ netflix_site/netflix_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # * allows the application to be hosted on any domain.
 
 
 # Application definition
@@ -143,11 +143,42 @@ STATICFILES_DIRS = [
 # Shows where the movies will be uploaded to
 MEDIA_URL = "media/"
 
+# MEDIA_ROOT is the directory where uploaded media files are stored.
+# Media files include user-uploaded content like profile pictures, document uploads, etc.
+# The path is set to a 'media' directory within the project's base directory.
+# Example: If BASE_DIR is "/home/user/myproject", MEDIA_ROOT will be "/home/user/myproject/media".
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# STATIC_ROOT is the directory where static files are collected to when running the 'collectstatic' management command.
+# Static files include JavaScript, CSS, images, and other files used to render the website.
+# The path is set to a 'staticfiles' directory within the project's base directory.
+# Example: If BASE_DIR is "/home/user/myproject", STATIC_ROOT will be "/home/user/myproject/staticfiles".
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CSRF_TRUSTED_ORIGINS is a list of trusted origins (domains) that are allowed to make
+# Cross-Site Request Forgery (CSRF) protected requests to this Django application.
+# This setting is particularly useful when you have your Django app hosted on multiple
+# domains or when making cross-origin requests from trusted domains.
+# Example: ['https://example.com', 'https://api.example.com']
+CSRF_TRUSTED_ORIGINS = []
+
+# CSRF_COOKIE_SECURE ensures that the CSRF cookie is only sent over HTTPS connections.
+# This setting should be enabled (set to True) in production environments to enhance security.
+# It helps prevent the CSRF token from being transmitted over unencrypted connections, reducing the risk of interception.
+CSRF_COOKIE_SECURE = True
+
+# SESSION_COOKIE_SECURE ensures that the session cookie is only sent over HTTPS connections.
+# This setting should be enabled (set to True) in production environments to enhance security.
+# It helps prevent session cookies from being transmitted over unencrypted connections, reducing the risk of session hijacking.
+SESSION_COOKIE_SECURE = True
+
+# SECURE_PROXY_SSL_HEADER is used when the Django application is behind a proxy (like a load balancer) that handles SSL termination.
+# This setting indicates which header the proxy uses to indicate that the request was originally made over HTTPS.
+# In this case, the proxy sets the "X-Forwarded-Proto" header to "https" when the request is secure.
+# This allows Django to properly detect secure requests and apply appropriate security measures.
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
